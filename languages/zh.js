7c161ddc3133115b30beca261f7c27c709d7279c
Wenzhao Zang <zangwz@gmail.com>
2015-05-02 01:56:42 -0400

    changed help page contents and size

--- languages/zh.js
+++ languages/zh.js
@@ -3,8 +3,8 @@
  */
 
 window.angularTranslationsLoaded('zh',{
-    "RULES_OF_HALATAFL":"欢迎来到Halatafl！游戏规则如下：",
-    "RULES_SLIDE1":"羊的目标是占领上方的3x3区域，如图所示。羊只能前进或左右移动，不能后退。",
+    "RULES_OF_HALATAFL":"欢迎来到Halatafl！",
+    "RULES_SLIDE1":"游戏规则：羊的目标是占领上方的3x3区域，如图所示。羊只能前进或左右移动，不能后退。",
     "RULES_SLIDE2":"狼的目标是抓住12只羊，从而阻止有足够数量的羊占领3x3区域。狼可以沿着网格向任意方向移动。当狼跳过羊时，羊被抓住。狼可以连跳，当狼有抓住羊的机会时，狼必须跳过这只羊，不能走其他方向。",
     "CLOSE":"关闭"
 });

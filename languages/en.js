da4b5df01b3c07289c39d64423fcd7fae1f4e5e3
Wenzhao Zang <zangwz@gmail.com>
2015-05-02 02:06:46 -0400

    changed help page contents and size

--- languages/en.js
+++ languages/en.js
@@ -4,6 +4,7 @@
 window.angularTranslationsLoaded('en',{
     "RULES_OF_HALATAFL":"Welcome to Halatafl!",
     "RULES_SLIDE1":"Rules: The goal of sheep is to occupy the 3x3 paddock area, as is shown here. Sheep can move only sideways and forwards but not backwards.",
-    "RULES_SLIDE2":"The goal of wolves is to catch 12 sheep to prevent them from filling the paddock area. Wolves can move in any direction along the game board's grid lines. To catch a sheep, wolves jump over them, one or more times. Wolves must jump if they can.",
+    "RULES_SLIDE2":"The goal of wolves is to catch 12 sheep to prevent them from filling the paddock area. Wolves can move in any direction along the game board's grid lines.",
+    "RULES_SLIDE3":"To catch a sheep, wolves jump over them, one or more times. Wolves must jump if they can.",
     "CLOSE":"Close"
 });
